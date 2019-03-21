ca3d4dbcedc7afdf8c50e3e23d3c7f271a843b0d
Diana <diana.sinenchenko@gmail.com>
2019-03-21 16:49:24 +0200

    MainScreenPresenter

---
+++ app/src/main/java/com/devitis/restapimvp/mainscreen/MainScreenPresenter.java
@@ -0,0 +1,12 @@
+package com.devitis.restapimvp.mainscreen;
+
+/**
+ * Created by Diana on 21.03.2019.
+ */
+
+public class MainScreenPresenter implements  IMainScreenContract.IPresenter{
+    @Override
+    public void loadPost() {
+
+    }
+}
