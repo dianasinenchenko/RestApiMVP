019c7cd1d7fcf889398e2d330fde2b0ef8d3341f
Diana <diana.sinenchenko@gmail.com>
2019-03-21 17:39:01 +0200

    update MainScreenPresenter

--- app/src/main/java/com/devitis/restapimvp/mainscreen/MainScreenPresenter.java
+++ app/src/main/java/com/devitis/restapimvp/mainscreen/MainScreenPresenter.java
@@ -5,7 +5,14 @@ import com.devitis.restapimvp.data.Post;
 import java.util.List;
 import java.util.Observable;
 
+import javax.inject.Inject;
+
+import retrofit2.Retrofit;
 import retrofit2.http.GET;
+import rx.Observer;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+//import rx.schedulers.Schedulers;
 
 
 /**
@@ -13,14 +20,39 @@ import retrofit2.http.GET;
  */
 
 public class MainScreenPresenter implements IMainScreenContract.IPresenter {
+
+    Retrofit retrofit;
+    IMainScreenContract.IView iView;
+
+    @Inject
+    public MainScreenPresenter(Retrofit retrofit, IMainScreenContract.IView iView) {
+        this.retrofit = retrofit;
+        this.iView = iView;
+    }
+
     @Override
     public void loadPost() {
+        retrofit.create(PostService.class).getPostList().subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .unsubscribeOn(Schedulers.io())
+                .subscribe(new Observer<List<Post>>() {
+                    @Override
+                    public void onCompleted() {
+                        iView.showComplete();
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        iView.showError(e.getMessage());
+                    }
+
+                    @Override
+                    public void onNext(List<Post> posts) {
+                        iView.showPosts(posts);
+                    }
+                });
 
     }
 
 
-    public interface IPostService {
-        @GET("/posts")
-        Observable<List<Post>> getPostList();
-    }
 }
