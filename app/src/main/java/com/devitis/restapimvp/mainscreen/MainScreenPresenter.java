efbae16091b57f6633a64530d77244b451a9333d
Diana <diana.sinenchenko@gmail.com>
2019-03-21 16:53:33 +0200

    IPostService

--- app/src/main/java/com/devitis/restapimvp/mainscreen/MainScreenPresenter.java
+++ app/src/main/java/com/devitis/restapimvp/mainscreen/MainScreenPresenter.java
@@ -1,12 +1,26 @@
 package com.devitis.restapimvp.mainscreen;
 
+import com.devitis.restapimvp.data.Post;
+
+import java.util.List;
+import java.util.Observable;
+
+import retrofit2.http.GET;
+
+
 /**
  * Created by Diana on 21.03.2019.
  */
 
-public class MainScreenPresenter implements  IMainScreenContract.IPresenter{
+public class MainScreenPresenter implements IMainScreenContract.IPresenter {
     @Override
     public void loadPost() {
 
+    }
+
+
+    public interface IPostService {
+        @GET("/posts")
+        Observable<List<Post>> getPostList();
     }
 }
